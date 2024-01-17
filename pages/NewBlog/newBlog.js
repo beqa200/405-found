95ae6e817b5400a48913595d8ac3dcb0432296a4
achiPhutkaradze <achifutkaradze9@gmail.com>
2024-01-18 02:41:19 +0400

    add image upload functionality

--- pages/NewBlog/newBlog.js
+++ pages/NewBlog/newBlog.js
@@ -1,1 +1,36 @@
-// const folderImage = document.getElementById("folder-add");
+const box = document.getElementById("box");
+const selectFileText = document.getElementById("selectFileText");
+const fileInput = document.getElementById("upload-image-input");
+const imagesLinkDiv = document.getElementById("div-for-file");
+const authorInput = document.getElementById("author-name-input");
+
+document.addEventListener("DOMContentLoaded", function () {
+  box.addEventListener("dragover", function (e) {
+    e.preventDefault();
+    box.classList.add("highlight");
+  });
+
+  box.addEventListener("dragleave", function () {
+    box.classList.remove("highlight");
+  });
+
+  box.addEventListener("drop", function (e) {
+    e.preventDefault();
+
+    box.classList.remove("highlight");
+
+    let files = e.dataTransfer.files;
+    console.log(files);
+    imagesLinkDiv.innerHTML = files[0].name;
+  });
+
+  selectFileText.addEventListener("click", function () {
+    fileInput.click();
+  });
+
+  fileInput.addEventListener("change", function () {
+    imagesLinkDiv.style.display = "flex";
+    imagesLinkDiv.innerHTML += fileInput.files[0].name;
+    box.style.display = "none";
+  });
+});
