0a299bad53f0cdbbe7aeac680f00affad971213e
achiPhutkaradze <achifutkaradze9@gmail.com>
2024-01-18 04:12:35 +0400

    add inputs validation

--- pages/NewBlog/newBlog.js
+++ pages/NewBlog/newBlog.js
@@ -3,6 +3,15 @@ const selectFileText = document.getElementById("selectFileText");
 const fileInput = document.getElementById("upload-image-input");
 const imagesLinkDiv = document.getElementById("div-for-file");
 const authorInput = document.getElementById("author-name-input");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
+const third = document.getElementById("third");
+const fourth = document.getElementById("least-4");
+const titleInput = document.getElementById("title-input");
+
+console.log(first);
+
+//  image upload functionality
 
 document.addEventListener("DOMContentLoaded", function () {
   box.addEventListener("dragover", function (e) {
@@ -21,7 +30,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     let files = e.dataTransfer.files;
     console.log(files);
-    imagesLinkDiv.innerHTML = files[0].name;
+    imagesLinkDiv.style.display = "flex";
+    imagesLinkDiv.innerHTML += files[0].name;
+    box.style.display = "none";
   });
 
   selectFileText.addEventListener("click", function () {
@@ -33,4 +44,41 @@ document.addEventListener("DOMContentLoaded", function () {
     imagesLinkDiv.innerHTML += fileInput.files[0].name;
     box.style.display = "none";
   });
+});
+
+// inputs validation
+
+function checkWhitespace() {
+  return /\s/.test(authorInput.value);
+}
+authorInput.addEventListener("input", function () {
+  for (let i = 0; i < authorInput.value.length; i++) {
+    if (i <= 4) {
+      first.style.color = "#EA1919";
+    } else {
+      first.style.color = "";
+    }
+  }
+  if (!checkWhitespace() && authorInput.value.trim() !== "") {
+    second.style.color = "#EA1919";
+  } else {
+    second.style.color = "";
+  }
+  let georgianRegex = /^[\u10A0-\u10EA\s]+$/;
+
+  if (georgianRegex.test(authorInput.value)) {
+    third.style.color = "";
+  } else {
+    third.style.color = "#EA1919";
+  }
+});
+
+titleInput.addEventListener("input", function ass() {
+  for (let i = 0; i < titleInput.value.length; i++) {
+    if (i <= 4) {
+      fourth.style.color = "#EA1919";
+    } else {
+      fourth.style.color = "";
+    }
+  }
 });
